4374e94dc875683fd7b7bc394b0f3651cec93db0
cjolley-lumen <cjolley@salesforce.com>
2012-06-21 12:27:25 -0600

    Working on Demo #3

--- src/main/java/platform/connect/api/SignedRequest.java
+++ src/main/java/platform/connect/api/SignedRequest.java
@@ -26,44 +26,26 @@ public class SignedRequest {
 
         String[] split = getParts(input);
 
-        String encoded_sig = split[0];
-        String encoded_envelope = split[1];
+        String encodedSig = split[0];
+        String encodedEnvelope = split[1];
 
         // Deserialize the json body
-        String json_envelope = new String(new Base64(true).decode(encoded_envelope));
+        String json_envelope = new String(new Base64(true).decode(encodedEnvelope));
         ObjectMapper mapper = new ObjectMapper();
         ObjectReader reader = mapper.reader(CanvasRequest.class);
         CanvasRequest canvasRequest;
+        String algorithm;
         try {
             canvasRequest = reader.readValue(json_envelope);
+            algorithm = canvasRequest.getAlgorithm() == null ? "HMACSHA256" : canvasRequest.getAlgorithm();
         } catch (IOException e) {
             throw new SecurityException(String.format("Error [%s] deserializing JSON to Object [%s]", e.getMessage(), CanvasRequest.class.getName()), e);
         }
 
-
-        SecretKey hmacKey = null;
-        String algorithm = null;
-        try {
-            algorithm = canvasRequest.getAlgorithm() == null ? "HMACSHA256" : canvasRequest.getAlgorithm();
-            byte[] key = secret.getBytes();
-            hmacKey = new SecretKeySpec(key, algorithm);
-            Mac mac = Mac.getInstance(algorithm);
-            mac.init(hmacKey);
-
-            // Check to see if the body was tampered with
-            byte[] digest = mac.doFinal(encoded_envelope.getBytes());
-            byte[] decode_sig = new Base64(true).decode(encoded_sig);
-            if (! Arrays.equals(digest, decode_sig)) {
-                String label = "Warning: Request was tampered with";
-                throw new SecurityException(label);
-            }
-        } catch (NoSuchAlgorithmException e) {
-            throw new SecurityException(String.format("Problem with algorithm [%s] Error [%s]", algorithm, e.getMessage()), e);
-        } catch (InvalidKeyException e) {
-            throw new SecurityException(String.format("Problem with key [%s] Error [%s]", hmacKey, e.getMessage()), e);
-        }
+        verify(secret, algorithm, encodedEnvelope, encodedSig);
 
         // If we got this far, then the request was not tampered with.
+        // return the request as a Java object
         return canvasRequest;
     }
 
@@ -72,54 +54,33 @@ public class SignedRequest {
 
         String[] split = getParts(input);
 
-        String encoded_sig = split[0];
-        String encoded_envelope = split[1];
+        String encodedSig = split[0];
+        String encodedEnvelope = split[1];
 
-        String json_envelope = new String(new Base64(true).decode(encoded_envelope));
+        String json_envelope = new String(new Base64(true).decode(encodedEnvelope));
         ObjectMapper mapper = new ObjectMapper();
 
+        String algorithm;
+        StringWriter writer;
         TypeReference<HashMap<String,Object>> typeRef
                 = new TypeReference<HashMap<String, Object>>() { };
         try {
-            HashMap<String,Object> o
-                    = mapper.readValue(json_envelope, typeRef);
-            StringWriter writer = new StringWriter();
+            HashMap<String,Object> o = mapper.readValue(json_envelope, typeRef);
+            writer = new StringWriter();
             mapper.writeValue(writer, o);
-
-            System.out.println("Hash Map " + o.toString());
-            System.out.println("Writer " + writer.toString());
+            algorithm = (String)o.get("algorithm");
+            //System.out.println("Writer " + writer.toString());
 
         } catch (IOException e) {
             throw new SecurityException(String.format("Error [%s] deserializing JSON to Object [%s]", e.getMessage(),
                     typeRef.getClass().getName()), e);
         }
 
-//        SecretKey hmacKey = null;
-//        String algorithm = null;
-//        try {
-//            algorithm = canvasRequest.getAlgorithm() == null ? "HMACSHA256" : canvasRequest.getAlgorithm();
-//            byte[] key = secret.getBytes();
-//            hmacKey = new SecretKeySpec(key, algorithm);
-//            Mac mac = Mac.getInstance(algorithm);
-//            mac.init(hmacKey);
-//
-//            // Check to see if the body was tampered with
-//            byte[] digest = mac.doFinal(encoded_envelope.getBytes());
-//            byte[] decode_sig = new Base64(true).decode(encoded_sig);
-//            if (! Arrays.equals(digest, decode_sig)) {
-//                String label = "Warning: Request was tampered with";
-//                throw new SecurityException(label);
-//            }
-//        } catch (NoSuchAlgorithmException e) {
-//            throw new SecurityException(String.format("Problem with algorithm [%s] Error [%s]", algorithm, e.getMessage()), e);
-//        } catch (InvalidKeyException e) {
-//            throw new SecurityException(String.format("Problem with key [%s] Error [%s]", hmacKey, e.getMessage()), e);
-//        }
-//
-//        // If we got this far, then the request was not tampered with.
-//        return canvasRequest;
+        verify(secret, algorithm, encodedEnvelope, encodedSig);
 
-        return "";
+        // If we got this far, then the request was not tampered with.
+        // return the request as a JSON string.
+        return writer.toString();
     }
 
     private static String[] getParts(String input) {
@@ -132,52 +93,29 @@ public class SignedRequest {
         return split;
     }
 
+    private static void verify(String secret, String algorithm, String encodedEnvelope, String encodedSig )
+        throws SecurityException
+    {
+        SecretKey hmacKey = null;
+        try {
+            byte[] key = secret.getBytes();
+            hmacKey = new SecretKeySpec(key, algorithm);
+            Mac mac = Mac.getInstance(algorithm);
+            mac.init(hmacKey);
 
-//
-//    @SuppressWarnings("unchecked")
-//    @Override
-//    public <T> T unsign(Class<T> clazz, String input, String secret) throws RuntimeException, SecurityException {
-//
-//        String[] split = input.split("[.]", 2);
-//
-//        String encoded_sig = split[0];
-//        String encoded_envelope = split[1];
-//
-//        SecretKey hmacKey = null;
-//        try {
-//            byte[] key = secret.getBytes();
-//            hmacKey = new SecretKeySpec(key, ALGORITHM.HMAC_SHA256.toString());
-//            Mac mac = Mac.getInstance(ALGORITHM.HMAC_SHA256.toString());
-//            mac.init(hmacKey);
-//
-//            // Check to see if the body was tampered with
-//            byte[] digest = mac.doFinal(encoded_envelope.getBytes());
-//            byte[] decode_sig = new Base64(true).decode(encoded_sig);
-//            if (! Arrays.equals(digest, decode_sig)) {
-//                String label = "Warning: Request was tampered with";
-//                throw new SecurityException(label);
-//            }
-//        } catch (NoSuchAlgorithmException e) {
-//            throw new RuntimException(String.format("Problem with algorithm [%s] Error [%s]", ALGORITHM.HMAC_SHA256.toString(), e.getMessage()), e);
-//        } catch (InvalidKeyException e) {
-//            throw new SignRequestException(String.format("Problem with key [%s] Error [%s]", hmacKey, e.getMessage()), e);
-//        }
-//
-//        // If we got this far, then the request was not tampered with.
-//
-//        String json_envelope = new String(new Base64(true).decode(encoded_envelope));
-//        ObjectMapper mapper = new ObjectMapper();
-//        ObjectReader reader = mapper.reader(clazz);
-//
-//        T object;
-//        try {
-//            object = (T)reader.readValue(json_envelope);
-//        } catch (IOException e) {
-//            throw new SignRequestException(String.format("Error [%s] deserializing JSON to Object [%s]", e.getMessage(), clazz.getName()), e);
-//        }
-//
-//        return object;
-//    }
-//
+            // Check to see if the body was tampered with
+            byte[] digest = mac.doFinal(encodedEnvelope.getBytes());
+            byte[] decode_sig = new Base64(true).decode(encodedSig);
+            if (! Arrays.equals(digest, decode_sig)) {
+                String label = "Warning: Request was tampered with";
+                throw new SecurityException(label);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new SecurityException(String.format("Problem with algorithm [%s] Error [%s]", algorithm, e.getMessage()), e);
+        } catch (InvalidKeyException e) {
+            throw new SecurityException(String.format("Problem with key [%s] Error [%s]", hmacKey, e.getMessage()), e);
+        }
 
+        // If we got here and didn't throw a SecurityException then all is good.
+    }
 }
--- src/main/webapp/examples/hello-world/index.jsp
+++ src/main/webapp/examples/hello-world/index.jsp
@@ -10,25 +10,41 @@
     }
     String yourConsumerSecret="3470819724070098100";
     SignedRequest.unsign(signedRequest[0], yourConsumerSecret);
-    SignedRequest.unsignToString(signedRequest[0], yourConsumerSecret);
+    String signedRequestJson = SignedRequest.unsignToString(signedRequest[0], yourConsumerSecret);
 %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
 
-<title>Hello World Canvas Example</title>
+    <title>Hello World Canvas Example</title>
 
-<link rel="stylesheet" type="text/css" href="/sdk/css/connect.css" />
-<!-- These can all be combined/compressed into a single file. -->
-<script type="text/javascript" src="/sdk/js/connect.js"></script>
-<script type="text/javascript" src="/sdk/js/xd.js"></script>
-<script type="text/javascript" src="/sdk/js/cookies.js"></script>
-<script type="text/javascript" src="/sdk/js/oauth.js"></script>
-<script type="text/javascript" src="/scripts/client.js"></script>
+    <link rel="stylesheet" type="text/css" href="/sdk/css/connect.css" />
 
+    <!-- These can all be combined/compressed into a single file. -->
+    <script type="text/javascript" src="/sdk/js/connect.js"></script>
+    <script type="text/javascript" src="/sdk/js/xd.js"></script>
+    <script type="text/javascript" src="/sdk/js/cookies.js"></script>
+    <script type="text/javascript" src="/sdk/js/oauth.js"></script>
+    <script type="text/javascript" src="/sdk/js/client.js"></script>
+
+    <!-- Third part libraries, substitute with your own -->
+    <script type="text/javascript" src="/scripts/json.js"></script>
+
+    <script>
+        if (self === top) {
+            // Not in Iframe
+            alert("This canvas app must be included within an iframe");
+        }
+
+        connect(function() {
+            var sr = JSON.parse('<%=signedRequestJson%>');
+            connect.byId('username').innerHTML = sr.fullName;
+        });
+
+    </script>
 </head>
 <body>
-    Hello World
+  Hello <span id='username'></span>
 </body>
 </html>
